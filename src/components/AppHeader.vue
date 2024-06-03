c641afbf8e33d3d06578854ecfcc81aaab4acd83
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-06-03 16:51:18 +0200

    app Header image path created

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,19 @@
 <script>
+
+export default {
+    name: 'AppHeader',
+    methods: {
+    createImagePath(img_name){
+        const url = new URL (`../assets/${img_name}`, import.meta.url);
+        return url.href
+    }
+}
+}
 </script>
 
 <template>
     <header class="flexsbc">
-        <img src="../assets/club-magellano-logo.svg" alt="Logo Club Magellano">
+        <img :src='createImagePath("club-magellano-logo.svg")' alt="Logo Club Magellano">
         <ul class="flexsbc">
             <li><a href="dashboard.html"><font-awesome-icon icon="cloud-sun" /></a></li>
             <li><a href="settings.html"><font-awesome-icon icon="gear" /></a></li>
