3e97f56cc5eeb4696b5d01879e3980d35091d84b
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-06-05 22:25:09 +0200

    404 added

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+	name: 'NotFoundPage',
+};
+</script>
+
+<template>
+	<div class="not-found">
+		<h3>404 | Page not found</h3>
+		<RouterLink class="btn" to="/">Torna alla Home</RouterLink>
+	</div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,16 @@ import {createRouter, createWebHistory} from 'vue-router';
 // Importo le mie pagine
 import DashboardPage from '../pages/DashboardPage.vue';
 import SettingsPage from '../pages/SettingsPage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 // Definisco le rotte
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', component: DashboardPage},
-        {path: '/settings', component: SettingsPage},
-        {path: '/:pathMatch(.*)*', redirect: '/'}
+        {path: '/', component: DashboardPage, name: 'dashboard'},
+        {path: '/settings', component: SettingsPage, name: 'settings'},
+
+        {path: '/not-found', component: NotFoundPage, name:'not-found'},
+        {path: '/:pathMatch(.*)*', redirect: 'not-found'}
     ]
 });
