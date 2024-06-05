c2b6be444a2b5a1554d2fd015b34f4d1791e3976
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-06-05 05:18:16 +0200

    methods for i and j, added icon, completed js logic

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -12,12 +12,34 @@ export default {
 		isLoading: false,
 		is5DOpen: false,
 		forecasts: [[]],
+		i: 0,
+		j: 0
 	}),
 	methods: {
 		// metodo per recuperare le immagini
 		createImagePath(img_name) {
 			const url = new URL(`../assets/${img_name}`, import.meta.url);
 			return url.href
+		},
+		// convertire Kelvin in Celsius
+		findTemperatureC(kelvin) {
+			return Math.round(kelvin - 273.15) + "°";
+		},
+		// prima lettera maiuscola
+		firstLetterUC(word) {
+			return word[0].toUpperCase() + word.slice(1);
+		},
+		// passa alle previsioni del giorno successivo
+		nextDay() {
+			if (this.i < 4) this.i++;
+		},
+		// passa alle previsioni del giorno precedente
+		previousDay() {
+			if (this.i > 0) this.i--;
+		},
+		// cambia l'orario a cui le previsioni fanno riferimento
+		changeHour(index) {
+			this.j = index;
 		},
 		// chiamata axios con attivazione e disattivazione loader\
 		fetchWeather() {
@@ -30,10 +52,25 @@ export default {
 					let sameDay = data.list[i].dt_txt.slice(0, 10);
 					// raccolgo i dati della chiamata axios in oggetti
 					data.list.forEach(item => {
-						const date = new Date(item.dt * 1000).toLocaleString();
-						const temp = (item.main.temp - 273.15).toFixed(2) + "°";
-						const weather = item.weather[0].description;
 						const today = item.dt_txt.slice(0, 10);
+						const date = new Date(item.dt * 1000).toLocaleString();
+						const temp = this.findTemperatureC(item.main.temp);
+						const weather = this.firstLetterUC(item.weather[0].description);
+						const pop = item.pop * 100 + "%";
+						let rainMM;
+						if (item.rain) {
+							rainMM = item.rain['3h'] + "mm";
+						} else {
+							rainMM = null;
+						};
+						const max = this.findTemperatureC(item.main.temp_max);
+						const min = this.findTemperatureC(item.main.temp_min);
+						const windSpeed = item.wind.speed + "m/s";
+						const windDirection = item.wind.deg;
+						const humidity = item.main.humidity + "%";
+						const pressure = item.main.pressure + " mbar";
+						const icon = item.weather[0].icon;
+
 						// verifico a che giorno appartengono e in caso il giorno sia cambiato creo e modifico l'array in cui pushare
 						if (today != sameDay) {
 							i++;
@@ -41,7 +78,7 @@ export default {
 							this.forecasts.push([]);
 						}
 						// push nell'array
-						this.forecasts[i].push({ date, temp, weather });
+						this.forecasts[i].push({ date, temp, weather, pop, rainMM, max, min, windSpeed, windDirection, humidity, pressure, icon });
 					});
 				})
 				// gestione errori
@@ -63,92 +100,78 @@ export default {
 
 <template>
 	<div v-if="!isLoading">
-		<h1>Previsioni Meteo:<br>Lodi</h1>
-		<!-- <h2 class="flexsec dates">
-			<div class="btn">
-				<< </div>
-					<div>{{ forecasts[0][0].date.slice(0, 5) }}</div>
-					<div class="btn">>></div>
-		</h2> -->
+		<!-- Titolo pagina -->
+		<h1>Previsioni Meteo: Lodi</h1>
+		<!-- Selettore data -->
+		<h2 class="flexsec dates">
+			<button class="btn" @click="previousDay()" :disabled="i <= 0">
+				<< </button>
+					<div>{{ forecasts[i][j].date.slice(0, 5) }}</div>
+					<button class="btn" @click="nextDay()" :disabled="i >= 4">>></button>
+		</h2>
+		<!-- Icona e main info -->
 		<div class="flexsec main-temp">
-			<img :src='createImagePath("club-magellano-image-logo.jpg")' alt="Icona Meteo" class="meteo-icon">
+			<img :src="`https://openweathermap.org/img/wn/${forecasts[i][j].icon}@2x.png`" alt="Icona Meteo"
+				class="meteo-icon">
+			<!-- <img :src='createImagePath("club-magellano-image-logo.jpg")' alt="Icona Meteo" class="meteo-icon"> -->
 			<div class="weatemp">
-				<div class="temperature">25°</div>
-				<div class="weather">Soleggiato</div>
-				<div class="max-min">22° / 16°</div>
+				<div class="temperature">{{ forecasts[i][j].temp }}</div>
+				<div class="weather">{{ forecasts[i][j].weather }}
+				</div>
+				<div class="max-min">{{ forecasts[i][j].max }} / {{ forecasts[i][j].min }}</div>
 			</div>
 		</div>
+		<!-- Previsioni a cinque giorni -->
 		<div>
 			<h2 class="accordion-button" @click="is5DOpen = !is5DOpen">
 				Previsioni a 5 giorni
 				<font-awesome-icon icon="chevron-down" />
 			</h2>
 			<ul v-show="is5DOpen">
-				<li class="flexsbc">
+				<li v-for="(forecast, index) in forecasts.slice(1, 5)" :key="index" class="flexsbc">
 					<div>
-						date
+						{{ forecast[0].date.slice(0, 5) }}
 					</div>
 					<div>
-						icon
+						{{ forecast[4].weather }}
 					</div>
 					<div>
-						min / max
-					</div>
-				</li>
-				<li class="flexsbc">
-					<div>
-						date
-					</div>
-					<div>
-						icon
-					</div>
-					<div>
-						min / max
-					</div>
-				</li>
-				<li class="flexsbc">
-					<div>
-						date
-					</div>
-					<div>
-						icon
-					</div>
-					<div>
-						min / max
-					</div>
-				</li>
-				<li class="flexsbc">
-					<div>
-						date
-					</div>
-					<div>
-						icon
-					</div>
-					<div>
-						min / max
+						{{ forecast[4].max }} / {{ forecasts[1][4].min }}
 					</div>
 				</li>
 			</ul>
 		</div>
+		<!-- Orari previsioni della giornata -->
 		<ul class="flexsec hours-container">
-			<li class="hours"><span class="btn">3:00</span></li>
-			<li class="hours"><span class="btn">3:00</span></li>
-			<li class="hours"><span class="btn">3:00</span></li>
-			<li class="hours"><span class="btn">3:00</span></li>
-			<li class="hours"><span class="btn">3:00</span></li>
-			<li class="hours"><span class="btn">3:00</span></li>
-			<li class="hours"><span class="btn">3:00</span></li>
-			<li class="hours"><span class="btn">3:00</span></li>
+			<li class="hours" v-for="(forecast, index) in forecasts[i]" :key="index" @click="changeHour(index)"><button
+					class="btn" :disabled="j == index">{{
+						forecast.date.slice(12,
+							17) }}</button></li>
 		</ul>
-		<ul>
-			<li>Pop: 80%</li>
-			<li>mm: 15</li>
-			<li>velocità vento: 30kmh</li>
-			<li>direzione vento: sud</li>
-			<li>umidità</li>
-			<li>pressione</li>
-			<li>visibilità</li>
-			<li>Alba / Tramonto</li>
-		</ul>
+		<!-- boxes con dettagli su temperatura -->
+		<div class="row flexsec">
+			<div class="col">
+				<div class="card">
+					<div>{{ forecasts[i][j].pop }}</div>
+					<div>{{ forecasts[i][j].rainMM }}</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div>{{ forecasts[i][j].windSpeed }}</div>
+					<div>{{ forecasts[i][j].windDirection }}</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div>{{ forecasts[i][j].humidity }}</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div>{{ forecasts[i][j].pressure }}</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
